e60c00e1eed8d8604116900a70c08df551b9a340
Daniel Garza <danielg272313@gmail.com>
2025-07-17 15:33:43 +0000

    remove bad children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className="bg-wanikani-dark">{children}
+            <body className="bg-wanikani-dark">
                 {children}
                 <Analytics />
             </body>
