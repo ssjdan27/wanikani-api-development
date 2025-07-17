da137833b21dddcca5f6489b01f17d5474e58ca4
Daniel Garza <danielg272313@gmail.com>
2025-07-17 15:41:10 +0000

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
     title: 'WaniKani Dashboard',
@@ -17,6 +18,7 @@ export default function RootLayout({
             <body className="bg-wanikani-dark">
                 {children}
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     )
